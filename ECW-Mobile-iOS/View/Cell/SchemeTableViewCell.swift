ffd631d6ff785062e3157918e4d09a035cd464f5
Muhammad Hassan Shakeel <hassan20896@Muhammads-Mac.local>
2019-07-22 06:13:30 -0700

    Add scheme cell

---
+++ ECW-Mobile-iOS/View/Cell/SchemeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SchemeTableViewCell.swift
+//  ECW-Mobile-iOS
+//
+//  Created by Muhammad Hassan Shakeel on 22/07/2019.
+//  Copyright © 2019 ECW. All rights reserved.
+//
+
+import UIKit
+
+class SchemeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
